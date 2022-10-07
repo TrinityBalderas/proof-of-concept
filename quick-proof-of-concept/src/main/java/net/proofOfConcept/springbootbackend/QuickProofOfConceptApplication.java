0e2bd835445774f1b92496b3ab1e23c642909254
Trinity Balderas <trinitybalderas02@gmail.com>
2022-10-07 17:35:19 -0500

    Initial commit

---
+++ quick-proof-of-concept/src/main/java/net/proofOfConcept/springbootbackend/QuickProofOfConceptApplication.java
@@ -0,0 +1,13 @@
+package net.proofOfConcept.springbootbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QuickProofOfConceptApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(QuickProofOfConceptApplication.class, args);
+	}
+
+}
---
+++ quick-proof-of-concept/src/test/java/net/proofOfConcept/springbootbackend/QuickProofOfConceptApplicationTests.java
@@ -0,0 +1,13 @@
+package net.proofOfConcept.springbootbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QuickProofOfConceptApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
